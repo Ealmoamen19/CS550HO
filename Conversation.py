4382516be5c31782795d0d14d05f5bf0b25deff9
Ealmoamen19 <43068192+Ealmoamen19@users.noreply.github.com>
2018-09-11 09:45:39 -0400

    Add files via upload

---
+++ Conversation.py
@@ -0,0 +1,13 @@
+print ("Hi there!")
+
+username = input("What's your name?")
+
+print("Nice to meet you " + username + "!")
+
+Help = input("How can I help you?")
+
+print("I'm sorry, I don't understand " + Help)
+
+yn = input("Do you want to try later?")
+
+print("Alright " + username +", I'll talk to you later")
