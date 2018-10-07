ac5f96fe24f8e8c2eea5b513fb206b46f4fec2d2
Eyad Almoamen19 <ealmoamen19@choate.edu>
2018-10-07 17:32:59 -0400

    update

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,8 +1,10 @@
 import random as rand
 
-row = 5
+row = 20
 
-col = 10
+col = 20
+
+cont = 0
 
 array = []
 
@@ -12,7 +14,7 @@ for i in range(0, row + 2) :
 
 count = 0
 
-mines = 40
+mines = 100
 
 while count < mines :
 
@@ -54,6 +56,56 @@ for i in range(row) :
 
  						array[a][z] = array[a][z] + 1
 
+def bloom (arr, rows, cols) :
+
+	if array[rows][cols] == 0 :
+
+		if rows < 1 :
+
+			rows = 1
+
+		elif rows > row:
+
+			rows = row
+
+		if cols < 1 :
+
+			cols = 1
+
+		elif cols > col:
+
+			cols = col
+
+		for i in range (rows - 1, rows + 2) :
+
+			for j in range (cols - 1, cols + 2) :
+
+				print("COL:")
+
+				print(cols, rows)
+
+				print()
+
+				print(i,j)
+
+				if array[i][j] == 0 and arr[i][j] == "o" :
+
+					arr[i][j] = array[i][j] 
+
+					global cont
+
+					cont = cont + 1
+
+					printer(arr)
+
+					print()
+
+					bloom (arr, i, j)
+
+				elif array[i][j] != "*" and arr[i][j] == "o" :
+					
+					arr[i][j] = array [i][j]  
+
 printer(array)
 
 print("\n\n")
@@ -76,7 +128,13 @@ while cont < ((row * col) - mines):
 
 	val = array[rowM][colM]
 
-	arrayM[rowM][colM] = val
+	if val == 0 :
+
+		bloom(arrayM, rowM, colM)
+
+	else:
+
+		arrayM[rowM][colM] = val
 
 	printer(arrayM)
 
